770b896947ad9d00e66db38455106ab6de9d9bae
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-25 11:53:55 +0100

    Restore inline verificiation of diff and use the local package install azuredevops pipeline
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/diffutils_works.rb
+++ controls/diffutils_works.rb
@@ -2,6 +2,7 @@ title 'Tests to confirm diffutils works as expected'
 
 plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'diffutils')
+hab_path = input('hab_path', value: '/tmp/hab')
 
 control 'core-plans-diffutils-works' do
   impact 1.0
@@ -10,15 +11,16 @@ control 'core-plans-diffutils-works' do
   Verify diffutils by ensuring 
   (1) its installation directory exists and 
   (2) that it returns the expected version.
-  (3) should detect equal contents
-  (4) should detect different content
+  (3) should detect equal contents and also different content.  Note
+  since the diff <(..) <(..) works in bash, not in sh, and command(..)
+  uses sh, then explicit use of bash(..) not command(..) was used to
+  do the verification
   '
   
-  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  plan_installation_directory = command("#{hab_path} pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
-    its('stderr') { should be_empty }
   end
   
   command_relative_path = input('command_relative_path', value: 'bin/diff')
@@ -28,20 +30,22 @@ control 'core-plans-diffutils-works' do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
     its('stdout') { should match /diff \(GNU diffutils\) #{plan_pkg_version}/ }
-    its('stderr') { should be_empty }
   end
 
-  alpha_file = "/hab/svc/diffutils/config/fixtures/alpha"
-  describe command("#{command_full_path} #{alpha_file} #{alpha_file}") do
+  describe bash("#{command_full_path} <(echo 'alpha' ) <(echo 'alpha')") do
     its('exit_status') { should eq 0 }
     its('stdout') { should be_empty }
-    its('stderr') { should be_empty }
   end
   
-  beta_file = "/hab/svc/diffutils/config/fixtures/beta"
-  describe command("#{command_full_path} #{alpha_file} #{beta_file}") do
+  expected = <<~EOF
+    1c1
+    < bob
+    ---
+    > harry
+  EOF
+  describe bash("#{command_full_path} <(echo 'bob') <(echo 'harry')") do
     its('exit_status') { should_not eq 0 }
     its('stdout') { should_not be_empty }
-    its('stderr') { should be_empty }
+    its('stdout') { should match /#{expected}/ }
   end
 end
